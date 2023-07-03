4d3779fd06048f778a3d76dff8b369b02800d9db
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-07-03 13:40:18 +0300

    обработка ошибок и коментарии

--- main.py
+++ main.py
@@ -6,25 +6,33 @@ from natsort import natsorted
 
 
 def create_pdf_from_photos(folder_path, output_folder):
+    # Собираем список путей к изображениям в выбранной папке и подпапках
     image_files = []
     for root, dirs, files in os.walk(folder_path):
         for filename in natsorted(files):
             if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
                 image_files.append(os.path.join(root, filename))
 
+    # Если нет фотографий, выводим сообщение и возвращаемся
     if len(image_files) == 0:
         print("В выбранной папке и ее подпапках нет фотографий.")
         return
 
+    # Создаем имя и путь для сохранения PDF-файла
     pdf_filename = os.path.join(output_folder, os.path.basename(folder_path) + '.pdf')
 
+    # Создаем список изображений для PDF-файла
     pdf_images = []
     for image_file in image_files:
         image = Image.open(image_file)
         pdf_images.append(image.convert('RGB'))
 
-    pdf_images[0].save(pdf_filename, save_all=True, append_images=pdf_images[1:])
-    print("PDF-файл успешно создан:", pdf_filename)
+    try:
+        # Сохраняем PDF-файл
+        pdf_images[0].save(pdf_filename, save_all=True, append_images=pdf_images[1:])
+        print("PDF-файл успешно создан:", pdf_filename)
+    except Exception as e:
+        print("Ошибка при создании PDF-файла:", str(e))
 
 
 def select_folder():
@@ -36,10 +44,15 @@ def select_folder():
         output_folder = os.path.join(parent_folder, os.path.basename(folder_path) + " PDF")
         os.makedirs(output_folder, exist_ok=True)
 
+        # Обрабатываем ошибки для каждой подпапки
         for root, dirs, files in os.walk(folder_path):
             for folder in dirs:
                 subfolder_path = os.path.join(root, folder)
-                create_pdf_from_photos(subfolder_path, output_folder)
+                try:
+                    create_pdf_from_photos(subfolder_path, output_folder)
+                except Exception as e:
+                    print("Ошибка при обработке папки:", subfolder_path)
+                    print(str(e))
 
 
 def main():

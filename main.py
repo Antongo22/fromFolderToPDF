f25eb14e71b3f848dcdb79dd9e0ba8da4e6fb8b3
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-07-24 23:10:34 +0300

    графика есть!

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def create_pdf_from_photos(folder_path, output_folder):
     # Если нет фотографий, выводим сообщение и возвращаемся
     if len(image_files) == 0:
         print("В выбранной папке и ее подпапках нет фотографий.")
+        # Очищаем содержимое текстового виджета
+        text_output.delete(1.0, tk.END)
+        text_output.insert(tk.END, "В выбранной папке и ее подпапках нет фотографий.")
+        text_output.update()
         return
 
     # Создаем имя и путь для сохранения PDF-файла
@@ -31,18 +35,30 @@ def create_pdf_from_photos(folder_path, output_folder):
         # Сохраняем PDF-файл
         pdf_images[0].save(pdf_filename, save_all=True, append_images=pdf_images[1:])
         print("PDF-файл успешно создан:", pdf_filename)
+        # Очищаем содержимое текстового виджета
+        text_output.delete(1.0, tk.END)
+        text_output.insert(tk.END, f"PDF-файл успешно создан: {pdf_filename}")
+        text_output.update()
     except Exception as e:
         print("Ошибка при создании PDF-файла:", str(e))
+        # Очищаем содержимое текстового виджета
+        text_output.delete(1.0, tk.END)
+        text_output.insert(tk.END, f"Ошибка при создании PDF-файла: {str(e)}")
+        text_output.update()
+
 
 
 def select_folder():
-    root = tk.Tk()
-    root.withdraw()
     folder_path = filedialog.askdirectory(title="Выберите папку с фотографиями")
     if folder_path:
         parent_folder = os.path.dirname(folder_path)
         output_folder = os.path.join(parent_folder, os.path.basename(folder_path) + " PDF")
         os.makedirs(output_folder, exist_ok=True)
+
+        # Очищаем содержимое текстового виджета
+        text_output.delete(1.0, tk.END)
+        text_output.insert(tk.END, "Начало создания PDF-файлов...\n")
+        text_output.update()
 
         # Обрабатываем ошибки для каждой подпапки
         for root, dirs, files in os.walk(folder_path):
@@ -51,13 +67,34 @@ def select_folder():
                 try:
                     create_pdf_from_photos(subfolder_path, output_folder)
                 except Exception as e:
-                    print("Ошибка при обработке папки:", subfolder_path)
-                    print(str(e))
+                    error_message = "Ошибка при обработке папки: {}\n{}".format(subfolder_path, str(e))
+                    text_output.insert(tk.END, error_message + "\n")
+                    text_output.update()
+
+        text_output.insert(tk.END, "Создание PDF-файлов завершено.\n")
+        text_output.see(tk.END)
 
 
 def main():
-    select_folder()
+    global text_output  # Объявляем text_output как глобальную переменную
+
+    root = tk.Tk()
+    root.title("Создание PDF из фотографий")
+    root.geometry("850x400")
+
+    def on_select_folder():
+        select_folder()
+
+    btn_select_folder = tk.Button(root, text="Выбрать папку", command=on_select_folder)
+    btn_select_folder.pack(pady=20)
+
+    text_output = tk.Text(root, wrap=tk.WORD, width=100, height=15)
+    text_output.pack(pady=10)
+
+    root.mainloop()
 
 
 if __name__ == '__main__':
     main()
+
+#добывать абстрактный класс для всех функций. от него унаследуй класс для папок. в нём реализовывается стандартный функционал.
